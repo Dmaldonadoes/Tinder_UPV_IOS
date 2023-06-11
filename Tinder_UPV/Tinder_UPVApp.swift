42d5533f2bab5ef13f912098c31eb9d850e5cfcc
Dmaldonadoes <dmaldonadoesteban@gmail.com>
2023-06-11 13:01:33 +0200

    Initial Commit

---
+++ Tinder_UPV/Tinder_UPVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_UPVApp.swift
+//  Tinder_UPV
+//
+//  Created by Diego Maldonado Esteban on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_UPVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
